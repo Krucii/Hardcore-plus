dae9e0ff928f249653fb52fc1c01dfd81d82769e
Krucii <26525897+Krucii@users.noreply.github.com>
2022-07-07 16:02:28 +0200

    Initial commit

---
+++ src/main/java/me/ziomki/hardcoreplus/HardcorePlus.java
@@ -0,0 +1,14 @@
+package me.ziomki.hardcoreplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HardcorePlus extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable is called!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable is called!");
+    }
+}
