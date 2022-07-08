8e13f4feae44f77324b1f458742b057cb685a5de
Nero404 <jakub.nowak404@gmail.com>
2022-07-08 13:11:22 +0200

    Test message

--- src/main/java/me/ziomki/hardcoreplus/HardcorePlus.java
+++ src/main/java/me/ziomki/hardcoreplus/HardcorePlus.java
@@ -3,6 +3,7 @@ package me.ziomki.hardcoreplus;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HardcorePlus extends JavaPlugin {
+
     @Override
     public void onEnable() {
         getLogger().info("onEnable is called!");
