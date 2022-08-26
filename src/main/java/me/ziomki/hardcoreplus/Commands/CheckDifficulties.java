c9915ee29e7797757f683a927a90cde3ac6f1dd1
Nero404 <jakub.nowak404@gmail.com>
2022-08-26 18:07:38 +0200

    Added option to show categories of difficulties

--- src/main/java/me/ziomki/hardcoreplus/Commands/CheckDifficulties.java
+++ src/main/java/me/ziomki/hardcoreplus/Commands/CheckDifficulties.java
@@ -9,6 +9,10 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class CheckDifficulties implements CommandExecutor {
 
     @Override
@@ -18,15 +22,33 @@ public class CheckDifficulties implements CommandExecutor {
         GUICreator gui = new GUICreator(p, 45, ChatColor.RED + "" + ChatColor.BOLD + "Piekielne utrudnienia");
         gui.fillGlass();
 
+        List<String> lore = new ArrayList<>();
+        lore.add("");
+        lore.add(ChatColor.GREEN + "Ta lista zawiera");
+        String what = "";
+        int size = DifficultiesList.getDifficultiesList().size();
+        if (strings.length > 0 && Objects.equals(strings[0], "procentowe")) {
+            what = "chance";
+            size = DifficultiesList.getDifficultiesChanceList().size();
+            lore.add(ChatColor.GREEN + "utrudnienia procentowe");
+        }
+        else if (strings.length > 0 && Objects.equals(strings[0], "permamentne")) {
+            what = "permament";
+            size = DifficultiesList.getDifficultiesPermamentList().size();
+            lore.add(ChatColor.GREEN + "utrudnienia permamentne");
+        }
+        else lore.add(ChatColor.GREEN + "wszystkie utrudnienia");
+
         for (int i = 1; i < 12; i++) {
-            ItemStack icon = DifficultiesList.createDiffList(i);
+            ItemStack icon = DifficultiesList.createDiffIcon(i, what);
             gui.addToGUI(icon);
         }
-        if (DifficultiesList.getDifficultiesList().size() > 11) {
-            gui.addPageInfo(1);
+        if (size > 11) {
+            gui.addPageInfo(1, lore);
             gui.addNextPage();
         }
         gui.displayInventory();
+
         return true;
     }
 
