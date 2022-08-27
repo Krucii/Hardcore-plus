a2b2e2c36d84d5cc97ab5e23dd29c4e6b730d12e
Nero404 <jakub.nowak404@gmail.com>
2022-08-27 22:23:43 +0200

    Small fixes

--- src/main/java/me/ziomki/hardcoreplus/Listeners/TwistedAnkleEvent.java
+++ src/main/java/me/ziomki/hardcoreplus/Listeners/TwistedAnkleEvent.java
@@ -5,7 +5,6 @@ import me.ziomki.hardcoreplus.Helpers.ActionBarMessage;
 import me.ziomki.hardcoreplus.Helpers.ChanceCalculator;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,30 +12,30 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-public class BrokenLegEvent implements Listener {
+public class TwistedAnkleEvent implements Listener {
 
-    DifficultiesList adding = new DifficultiesList(5.0, Material.DIRT, org.bukkit.ChatColor.GREEN, "Łamaga", "Pojawia się rosnąca wraz z wysokością szansa na złamanie nogi podczas upadku.");
+    DifficultiesList adding = new DifficultiesList(5.0, Material.DIRT, org.bukkit.ChatColor.GREEN, "Łamaga", "Pojawia się rosnąca wraz z wysokością szansa na skręcenie kostki podczas upadku.");
 
     @EventHandler
     public void onPlayerFall(EntityDamageEvent e) {
-        if (e.getEntity().getType() == EntityType.PLAYER) {
+        if (e.getEntity() instanceof Player p) {
             if (e.getCause() == EntityDamageEvent.DamageCause.FALL) {
-                float fallDistance = e.getEntity().getFallDistance();
+                float fallDistance = p.getFallDistance();
                 if (fallDistance < 5) {
                     if (ChanceCalculator.getChance(adding.getChance()))
-                        boneBreak((Player) e.getEntity(), 40*20, 2);
+                        boneBreak(p, 40*20, 2);
                 }
                 else if (fallDistance < 8 && fallDistance > 5) {
                     if (ChanceCalculator.getChance(adding.getChance() * 4))
-                        boneBreak((Player) e.getEntity(), 80*20, 2);
+                        boneBreak(p, 80*20, 2);
                 }
                 else if (fallDistance < 12 && fallDistance > 8) {
                     if (ChanceCalculator.getChance(adding.getChance() * 8))
-                        boneBreak((Player) e.getEntity(), 160*20, 2);
+                        boneBreak(p, 160*20, 2);
                 }
                 else {
                     if (ChanceCalculator.getChance(adding.getChance() * 16))
-                        boneBreak((Player) e.getEntity(), 320*20, 3);
+                        boneBreak(p, 320*20, 3);
                 }
             }
         }
@@ -44,6 +43,6 @@ public class BrokenLegEvent implements Listener {
 
     public void boneBreak(Player p, int dur, int ampl) {
         p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, dur, ampl));
-        ActionBarMessage.sendActionBarMessage(p, ChatColor.RED + "Zlamales noge");
+        ActionBarMessage.sendActionBarMessage(p, ChatColor.RED + "Złamałeś nogę");
     }
 }
