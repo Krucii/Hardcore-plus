bb1cb1dad3347bdd40144fc3bdf2e4c4ed16e8e6
Krucii <26525897+Krucii@users.noreply.github.com>
2023-02-24 15:58:18 +0100

    1 line removal

--- src/main/java/me/ziomki/hardcoreplus/Modules/PlayerDeath/DeathEraseItems.java
+++ src/main/java/me/ziomki/hardcoreplus/Modules/PlayerDeath/DeathEraseItems.java
@@ -11,7 +11,6 @@ public class DeathEraseItems extends PluginModule {
     @Override
     public void onEvent(Event e) {
         PlayerDeathEvent event = (PlayerDeathEvent) e;
-        System.out.println("Event occured");
         for (int i = 0; i < event.getDrops().size(); i++) {
             if (ChanceCalculator.getChance(50))
                 event.getDrops().set(i, new ItemStack(Material.AIR));
